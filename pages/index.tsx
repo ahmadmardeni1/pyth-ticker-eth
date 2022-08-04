082264206d780b900bb3b9857f9f2450594c38d6
Ahmad Mardeni <ahmadmardeni0@gmail.com>
2022-08-04 19:31:34 +0400

    ETH Ticker

---
+++ pages/index.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { PythConnection } from "@pythnetwork/client";
+
+const Home: NextPage = () => {
+  const [solPrice, setSolPrice] = useState(0);
+  const [solConfidence, setSolConfidence] = useState(0);
+
+  useEffect(() => {
+    const connection = new Connection("http://api.mainnet-beta.solana.com");
+    const programKey = new PublicKey(
+      "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB"
+    );
+    const pythConnection = new PythConnection(connection, programKey);
+
+    pythConnection.onPriceChange((product, price) => {
+      if (
+        product.symbol == "Crypto.ETH/USD" &&
+        price.price &&
+        price.confidence
+      ) {
+        setSolPrice(price.price);
+        setSolConfidence(price.confidence);
+      }
+    });
+
+    pythConnection.start();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ticker</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Pyth Ticker</h1>
+        <h2>ETH/USD</h2>
+        <h3>
+          {solPrice} &plusmn;{solConfidence}
+        </h3>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
